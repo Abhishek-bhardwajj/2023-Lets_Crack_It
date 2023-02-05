bf270d611b67b2bc2c119657cf45e811e9be8be1
abhishek6252 <abhishek.bhardwaj_cs20@gla.ac.in>
2023-02-05 23:51:41 +0530

    LinkedList_Queue

---
+++ src/collectionFrameworks/LinkedList_Queue.java
@@ -0,0 +1,21 @@
+package collectionFrameworks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+public class LinkedList_Queue {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+
+        System.out.println(queue);
+
+        System.out.println(queue.poll());
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+    }
+}
